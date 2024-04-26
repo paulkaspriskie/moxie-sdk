f06312a8e7bdd266e5ea0aaa70791fe15192df7b
Paul <paulkaspriskie@gmail.com>
2024-04-26 11:31:19 -0400

    refactor(build/start): replaced dynamic build logic build.js with build.sh.

---
+++ lib/util/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "${1}" ]; then
+  CLI_INPUT='moxie-wiki'
+else
+  CLI_INPUT=$1
+fi
+
+echo $CLI_INPUT
+NODE_ENV=production node_modules/.bin/webpack --config ./lib/config/webpack.config.js --entry ./apps/$CLI_INPUT/src/js/index.js
