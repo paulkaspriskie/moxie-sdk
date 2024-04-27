ac689b743b33bd98c5fcb2c14197956685275fd8
Paul <paulkaspriskie@gmail.com>
2024-04-27 15:37:10 -0400

    feat(build): added logic to sanitize & extract app name from file path.

--- lib/util/build.sh
+++ lib/util/build.sh
@@ -2,8 +2,7 @@
 
 FILE_BUILD=$PWD/lib/util/build.sh
 DIR_APPS=$PWD/apps
-DEFAULT_APP=$(find $DIR_APPS -maxdepth 1 -mindepth 1 -type d | head -n 1)
-
+DEFAULT_APP=$(find $DIR_APPS -maxdepth 1 -mindepth 1 -type d | head -n 1 | sed 's:.*/::')
 
 if [ ! -x "$FILE_BUILD" ]; then
   chmod +x $FILE_BUILD
@@ -15,4 +14,4 @@ else
   CLI_INPUT=$1
 fi
 
-NODE_ENV=production node_modules/.bin/webpack --config ./lib/config/webpack.config.js --entry ./apps/$CLI_INPUT/src/js/index.js
+# NODE_ENV=production node_modules/.bin/webpack --config ./lib/config/webpack.config.js --entry ./apps/$CLI_INPUT/src/js/index.js
