ff9de28053a5f4280bbc31e102891639c98fdfaf
Paul <paulkaspriskie@gmail.com>
2024-04-27 14:56:30 -0400

    feat(build): added preliminary logic to define default app

--- lib/util/build.sh
+++ lib/util/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 FILE_BUILD=$PWD/lib/util/build.sh
+DIR_APPS=$PWD/apps
+DEFAULT_APP=$(find $DIR_APPS -maxdepth 1 -mindepth 1 -type d | head -n 1)
+
 
 if [ ! -x "$FILE_BUILD" ]; then
   chmod +x $FILE_BUILD
