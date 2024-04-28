b8f19c023fdd718fc07830d9109c734843537d43
Paul <paulkaspriskie@gmail.com>
2024-04-28 08:48:42 -0400

    feat(build): implemented dynamic default build logic.

--- lib/util/build.sh
+++ lib/util/build.sh
@@ -2,16 +2,16 @@
 
 FILE_BUILD=$PWD/lib/util/build.sh
 DIR_APPS=$PWD/apps
-DEFAULT_APP=$(find $DIR_APPS -maxdepth 1 -mindepth 1 -type d | head -n 1 | sed 's:.*/::')
+
 
 if [ ! -x "$FILE_BUILD" ]; then
   chmod +x $FILE_BUILD
 fi
 
 if [ -z "${1}" ]; then
-  CLI_INPUT='moxie-wiki'
+  CLI_INPUT=$(find $DIR_APPS -maxdepth 1 -mindepth 1 -type d | head -n 1 | sed 's:.*/::')
 else
   CLI_INPUT=$1
 fi
 
-# NODE_ENV=production node_modules/.bin/webpack --config ./lib/config/webpack.config.js --entry ./apps/$CLI_INPUT/src/js/index.js
+NODE_ENV=production node_modules/.bin/webpack --config ./lib/config/webpack.config.js --entry ./apps/$CLI_INPUT/src/js/index.js
