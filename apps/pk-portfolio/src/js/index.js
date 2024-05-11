051e4216297c95ee79cadb1b3811095088b24c6f
Paul <paulkaspriskie@gmail.com>
2024-05-11 09:36:47 -0400

    fix(pk-portfolio) corrected formatting issues and updated styles.

--- apps/pk-portfolio/src/js/index.js
+++ apps/pk-portfolio/src/js/index.js
@@ -1,53 +1,67 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { createRoot } from 'react-dom/client';
 import pkLogo from '../assets/pk-crane.svg';
 import "../scss/index.scss";
 
 const One = () => {
-	return (
-		<div><h1>One</h1></div>
-	)
+  return (
+    <div></div>
+  )
 }
 
-const Two = () => {
-	return (
-		<div><h1>Two</h1></div>
-	)
+const Skills = () => {
+  return (
+    <div>
+      <div><h1>Skills</h1></div>
+    </div>
+  )
 }
 
-const Three = () => {
-	return (
-		<div><h1>Three</h1></div>
-	)
+const Projects = () => {
+  return (
+    <div>
+      <div><h1>Projects</h1></div>
+    </div>
+  )
+}
+
+const Contact = () => {
+  return (
+    <div>
+      <div><h1>Contact</h1></div>
+    </div>
+  );
 }
 
 const App = () => {
-	const location = useLocation();
+  const location = useLocation();
 
-	return (
-		<div className="app-pk-portfolio__container">
-			<nav>
-				<ul>
-					<li><img src={pkLogo} alt="React Logo" /><Link to="/">Paul Kaspriskie</Link></li>
-					<li><Link to="/two">Two</Link></li>
-					<li><Link to="/three">Three</Link></li>
-				</ul>
-			</nav>
-			<main>
-				<Routes>
-					<Route index element={<One/>} />
-					<Route path="two" element={<Two/>} />
-					<Route path="three" element={<Three/>} />
-				</Routes>
-			</main>
-		</div>
-	)
+  return (
+    <div className="app-pk-portfolio__container">
+      <nav>
+        <ul>
+          <li><img src={pkLogo} alt="React Logo" /><Link to="/">Paul Kaspriskie</Link></li>
+          <li><Link to="/skills">Skills</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route index element={<One/>} />
+          <Route path="skills" element={<Skills/>} />
+          <Route path="projects" element={<Projects/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 const root = createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<App/>
-	</BrowserRouter>
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
