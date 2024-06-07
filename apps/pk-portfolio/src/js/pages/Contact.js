549337f69f2a6ff6928b7119c7789963360b4263
Paul <paulkaspriskie@gmail.com>
2024-06-07 13:49:54 -0400

    feat(pk-portfolio): added Hero component to Contact and Projects page component jsx.

--- apps/pk-portfolio/src/js/pages/Contact.js
+++ apps/pk-portfolio/src/js/pages/Contact.js
@@ -1,8 +1,13 @@
+import Hero from '../components/Hero.js';
+
 
 const Contact = () => {
   return (
     <div className="layout-page__contact">
-      <h1>Contact</h1>
+      <Hero>
+        <h1>Contact</h1>
+				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
+      </Hero>
     </div>
   );
 }
--- apps/pk-portfolio/src/js/pages/Projects.js
+++ apps/pk-portfolio/src/js/pages/Projects.js
@@ -1,8 +1,13 @@
+import Hero from '../components/Hero.js';
+
 
 const Projects = () => {
   return (
     <div className="layout-page__projects">
-      <h1>Projects</h1>
+      <Hero>
+        <h1>Projects</h1>
+				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
+      </Hero>
     </div>
   )
 }
