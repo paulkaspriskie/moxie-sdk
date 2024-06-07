f15c9380f82c79c661338378b16e2b0aedcdfb8f
Paul <paulkaspriskie@gmail.com>
2024-06-07 13:27:40 -0400

    feat(pk-portfolio): added containment logic to Hero ui component to improve content scalability & dynamism.

--- apps/pk-portfolio/src/js/components/Hero.js
+++ apps/pk-portfolio/src/js/components/Hero.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ParticleLayer from '../components/ParticleLayer.js';
 
-const Hero = () => {
+
+const Hero = (props) => {
 	return (
 		<div className="component-hero__container">
-			<section>
-				<h1>Experience</h1>
-				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
-			</section>
+			<section>{props.children}</section>
 		</div>
 	)
 }
--- apps/pk-portfolio/src/js/pages/Experience.js
+++ apps/pk-portfolio/src/js/pages/Experience.js
@@ -4,7 +4,10 @@ import Hero from '../components/Hero.js';
 const Experience = () => {
   return (
     <div className="layout-page__experience">
-      <Hero/>
+      <Hero>
+        <h1>Experience</h1>
+				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
+      </Hero>
     </div>
   )
 }
