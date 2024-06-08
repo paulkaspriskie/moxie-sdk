00f78f0eb08f2a1691a59d1af6f6845be4ac075f
Paul <paulkaspriskie@gmail.com>
2024-06-08 17:24:14 -0400

    fix(pk-portfolio): corrected classname typo in skills page component.

--- apps/pk-portfolio/src/js/pages/Skills.js
+++ apps/pk-portfolio/src/js/pages/Skills.js
@@ -17,7 +17,7 @@ const Skills = () => {
 
 
   return (
-    <div className="layout-page__skils">
+    <div className="layout-page__skills">
       <Hero>
         <h1>Skills</h1>
 				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
