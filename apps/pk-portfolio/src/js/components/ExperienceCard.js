14d9450d7a792011c92db606430d4b5b33a022df
Paul <paulkaspriskie@gmail.com>
2024-06-09 13:33:06 -0400

    refactor(pk-portfolio): renamed ui component ExperienceCard~>VerticalTimeline to better represent usecase.

--- apps/pk-portfolio/src/js/components/ExperienceCard.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ExperienceCard = () => {
-	return <div className="component-experience-card__container"><h1>experience-card</h1></div>
-}
-
-export default ExperienceCard;
---
+++ apps/pk-portfolio/src/js/components/VerticleTimeline.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VerticleTimeline = () => {
+	return <div className="component-vertical-timeline__container"><h1>experience-card</h1></div>
+}
+
+export default VerticalTimeline;
