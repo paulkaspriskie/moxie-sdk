a8a70d7a101df7242b48adffc7c9878f210701d2
Paul <paulkaspriskie@gmail.com>
2024-06-09 19:30:21 -0400

    refactor(pk-portfolio): updated data/markup in VerticalTimeline within Experience page component .

--- apps/pk-portfolio/src/js/components/VerticleTimeline.js
+++ apps/pk-portfolio/src/js/components/VerticleTimeline.js
@@ -12,6 +12,13 @@ const VerticleTimeline = () => {
 					<span>Barclays</span>
 					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
 				</li>
+				<li>
+					<div></div>
+					<time>October 2015 - August 2021</time>
+					<h3>Web Developer</h3>
+					<span>Temple University</span>
+					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
+				</li>
 			</ul>
 		</div>
 	);
