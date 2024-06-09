54a8df8e32f6218ad0dbf6785576d9ec45eaf071
Paul <paulkaspriskie@gmail.com>
2024-06-09 14:21:18 -0400

    feat(pk-portfolio): added rudimentary markup and styles to VerticalTimeline ui component.

--- apps/pk-portfolio/src/js/components/VerticleTimeline.js
+++ apps/pk-portfolio/src/js/components/VerticleTimeline.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+
 const VerticleTimeline = () => {
-	return <div className="component-vertical-timeline__container"><h1>experience-card</h1></div>
+	return (
+		<div className="component-verticle-timeline__container">
+			<ul>
+				<li>
+					<time>September 2021 - Present</time>
+					<h3>UI Developer</h3>
+					<span>Barclays</span>
+					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
-export default VerticalTimeline;
+export default VerticleTimeline;
--- apps/pk-portfolio/src/js/pages/Experience.js
+++ apps/pk-portfolio/src/js/pages/Experience.js
@@ -1,4 +1,5 @@
 import Hero from '../components/Hero.js';
+import VerticleTimeline from '../components/VerticleTimeline.js';
 
 
 const Experience = () => {
@@ -8,6 +9,7 @@ const Experience = () => {
         <h1>Experience</h1>
 				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
       </Hero>
+      <VerticleTimeline />
     </div>
   )
 }
