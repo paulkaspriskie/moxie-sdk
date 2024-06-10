14e525236decb3523b9c6951e46f95551224802a
Paul <paulkaspriskie@gmail.com>
2024-06-10 10:31:43 -0400

    feat(pk-portfolio): modified markup of ui component VerticleTime to include location element and added corresponding styles.

--- apps/pk-portfolio/src/js/components/VerticleTimeline.js
+++ apps/pk-portfolio/src/js/components/VerticleTimeline.js
@@ -10,6 +10,7 @@ const VerticleTimeline = () => {
 					<time>September 2021 - Present</time>
 					<h3>UI Developer</h3>
 					<span>Barclays</span>
+					<span>Wilmington, Delaware</span>
 					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
 				</li>
 				<li>
@@ -17,6 +18,7 @@ const VerticleTimeline = () => {
 					<time>October 2015 - August 2021</time>
 					<h3>Web Developer</h3>
 					<span>Temple University</span>
+					<span>Philadelphia, Pennsylvania</span>
 					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
 				</li>
 			</ul>
