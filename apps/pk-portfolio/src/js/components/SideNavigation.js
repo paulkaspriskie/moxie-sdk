f97abcc0d25ebcdabdccd4a0fe6ca507fdae4e94
Paul <paulkaspriskie@gmail.com>
2024-06-11 21:32:16 -0400

    refactor(pk-portfolio): replaced static svg import with ui component Icon to supply dynamic svgs & updated SideNav styles .

--- apps/pk-portfolio/src/js/components/SideNavigation.js
+++ apps/pk-portfolio/src/js/components/SideNavigation.js
@@ -15,8 +15,8 @@ const SideNavigation = ({routes}) => {
                 className={({ isActive }) => (isActive ? 'active' : undefined)}
                 end>
                 {
-                  index === 0 ? <Icon name={'pk-crane-icon'} /> :
-                  <img src={require(`../../assets/icons/${route.name.toLowerCase()}-icon.svg`)} />
+                  index === 0 ? <img src={require(`../../assets/icons/pk-crane-icon.png`)} /> :
+                  <Icon name={`${route.name.toLowerCase()}-icon`} />
                 }
                 {
                   index === 0 ? <h3>{route.name}<span>Frontend / UI Developer</span></h3> : route.name
@@ -28,7 +28,7 @@ const SideNavigation = ({routes}) => {
         <ul>
           {
             appData.navSocial.map((item, i) => {
-              return <li key={i}><a href={item.url} target="_blank"><img src={require(`../../assets/icons/${item.fileName}`)} /></a></li>
+              return <li key={i}><a href={item.url} target="_blank"><Icon name={item.fileName} /></a></li>
             })
           }
         </ul>
