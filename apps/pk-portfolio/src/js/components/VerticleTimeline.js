331eeff277f4720ceb425e57de9b23bd8a1baff1
Paul <paulkaspriskie@gmail.com>
2024-06-13 10:19:32 -0400

    refactor(pk-portfolio): added containment logic to ui component VerticleTimeline and moved corresponding markup to Experience component.

--- apps/pk-portfolio/src/js/components/VerticleTimeline.js
+++ apps/pk-portfolio/src/js/components/VerticleTimeline.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
 
-const VerticleTimeline = () => {
-	return (
-		<div className="component-verticle-timeline__container">
-			<ul>
-				<li>
-					<div></div>
-					<time>September 2021 - Present</time>
-					<h3>UI Developer</h3>
-					<span>Barclays</span>
-					<span>Wilmington, Delaware</span>
-					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
-				</li>
-				<li>
-					<div></div>
-					<time>October 2015 - August 2021</time>
-					<h3>Web Developer</h3>
-					<span>Temple University</span>
-					<span>Philadelphia, Pennsylvania</span>
-					<p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
-				</li>
-			</ul>
-		</div>
-	);
+const VerticleTimeline = (props) => {
+  return (
+    <div className="component-verticle-timeline__container">
+      <ul>
+        {props.children}
+      </ul>
+    </div>
+  );
 }
 
 export default VerticleTimeline;
--- apps/pk-portfolio/src/js/pages/Experience.js
+++ apps/pk-portfolio/src/js/pages/Experience.js
@@ -9,7 +9,24 @@ const Experience = () => {
         <h1>Experience</h1>
 				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
       </Hero>
-      <VerticleTimeline />
+      <VerticleTimeline>
+        <li>
+          <div></div>
+          <time>September 2021 - Present</time>
+          <h3>UI Developer</h3>
+          <span>Barclays</span>
+          <span>Wilmington, Delaware</span>
+          <p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
+        </li>
+        <li>
+          <div></div>
+          <time>October 2015 - August 2021</time>
+          <h3>Web Developer</h3>
+          <span>Temple University</span>
+          <span>Philadelphia, Pennsylvania</span>
+          <p>Roquefort rubber cheese boursin. Cow caerphilly monterey jack cheese and biscuits cheesy feet stinking bishop gouda hard cheese. Cheesecake fromage camembert de normandie who moved my cheese cheesy grin stinking bishop cheesy grin smelly cheese</p>
+        </li>
+      </VerticleTimeline>
     </div>
   )
 }
