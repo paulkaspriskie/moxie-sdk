eda6135e65981b4263aa07d06e0f1f0862449ff2
Paul <paulkaspriskie@gmail.com>
2024-06-14 15:07:33 -0400

    refactor(pk-portfolio): modified ui component Tabs, refactored logic to be more dynamic.

--- apps/pk-portfolio/src/js/components/Tabs.js
+++ apps/pk-portfolio/src/js/components/Tabs.js
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
 
 
-const Tabs = () => {
+const Tabs = ({data}) => {
 
   const [selectedTabIndex, setSelectedTabIndex ] = useState(0);
-
-  const tabs = [
-    { id: 'tab1', title: 'Core Skills', content: <p>Core Skills</p> },
-    { id: 'tab2', title: 'Javascript', content: <p>Javascript</p> },
-    { id: 'tab3', title: 'CSS', content: <p>CSS</p> },
-  ];
-
 
   return (
     <div className="component-tabs__container">
       <ul>
         {
-          tabs.map((item, i) => {
+          data.map((item, i) => {
             return (
               <li key={i}>
                 <a href="#" onClick={() => setSelectedTabIndex(i)} className={i === selectedTabIndex ? 'active' : ''}>
@@ -28,7 +21,7 @@ const Tabs = () => {
         }
       </ul>
       <section>
-        {tabs[selectedTabIndex].content}
+        {data[selectedTabIndex].content}
       </section>
     </div>
   );
--- apps/pk-portfolio/src/js/pages/Skills.js
+++ apps/pk-portfolio/src/js/pages/Skills.js
@@ -3,6 +3,13 @@ import Tabs from '../components/Tabs.js';
 
 
 const Skills = () => {
+
+  const tabs = [
+    { id: 'tab1', title: 'Core Skills', content: <p>Core Skills</p> },
+    { id: 'tab2', title: 'Javascript', content: <p>Javascript</p> },
+    { id: 'tab3', title: 'CSS', content: <p>CSS</p> },
+  ];
+
   return (
     <div className="layout-page__skills">
       <Hero>
@@ -10,7 +17,9 @@ const Skills = () => {
 				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
       </Hero>
       <div>
-        <Tabs />
+        <Tabs data={tabs}>
+          hello
+        </Tabs>
       </div>
     </div>
   )
