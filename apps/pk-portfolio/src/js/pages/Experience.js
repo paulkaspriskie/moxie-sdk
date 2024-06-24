f2d60be5a54cb676afc720f6201ad0c1d4858088
Paul <paulkaspriskie@gmail.com>
2024-06-24 11:28:35 -0400

    refactor(pk-portfolio): modified page component Experience, reconfigured markup/styles to be consistent with all other page componets.

--- apps/pk-portfolio/src/js/pages/Experience.js
+++ apps/pk-portfolio/src/js/pages/Experience.js
@@ -9,18 +9,20 @@ const Experience = () => {
         <h1>Experience</h1>
 				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
       </Hero>
-      <VerticleTimeline>
-        {appData.experience.map((item, i) =>
-          <li key={i}>
-            <div></div>
-            <time>{item.date}</time>
-            <h3>{item.role}</h3>
-            <span>{item.company}</span>
-            <span>{item.location}</span>
-            <p>{item.description}</p>
-          </li>
-        )}
-      </VerticleTimeline>
+      <div>
+        <VerticleTimeline>
+          {appData.experience.map((item, i) =>
+            <li key={i}>
+              <div></div>
+              <time>{item.date}</time>
+              <h3>{item.role}</h3>
+              <span>{item.company}</span>
+              <span>{item.location}</span>
+              <p>{item.description}</p>
+            </li>
+          )}
+        </VerticleTimeline>
+      </div>
     </div>
   )
 }
