db79e598d9dae1a0b3641367e03ae96040a9aae1
Paul <paulkaspriskie@gmail.com>
2024-07-12 12:11:11 -0400

    refactor(pk-portfolio): modified ~/pages/Experience, reversed position of download icon to appear after link text.

--- apps/pk-portfolio/src/js/pages/Experience.js
+++ apps/pk-portfolio/src/js/pages/Experience.js
@@ -10,7 +10,7 @@ const Experience = () => {
       <Hero>
         <h1>Experience</h1>
 				<p>This some paragraph text, not much to see here. Words are cool and I say them sometimes. Some more words for good measures.</p>
-        <a href={resume} target="_blank"><Icon name={'download-icon'} />Download Resume</a>
+        <a href={resume} target="_blank">Download Resume<Icon name={'download-icon'} /></a>
       </Hero>
       <div className={'layout-page__content'}>
         <VerticleTimeline>
