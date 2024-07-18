14a8d0c7fbee30cd15908ae7464e6eff60eabc78
Paul <paulkaspriskie@gmail.com>
2024-07-18 08:05:44 -0400

    feat(pk-portfolio): modified ~/pages/Home, replaced static text content with api call.

--- apps/pk-portfolio/src/js/pages/Home.js
+++ apps/pk-portfolio/src/js/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import ParticleLayer from '../components/ParticleLayer.js';
+import appData from '../api/data-app.json';
 
 
 const Home = () => {
@@ -8,8 +9,8 @@ const Home = () => {
       <ParticleLayer />
       <div className="layout-page__home">
         <div><img src={require("../../assets/imgs/pk-profile.png")}/></div>
-        <h1>Hello there, I'm Paul Kaspriskie.</h1>
-        <p>An accomplished Philadelphia based Frontend / UI Developer with over eight years of experience. Proficiencies in developing, maintaining and extending UI components to support underlying design systems / web applications.</p>
+        <h1>{appData.bio.title}</h1>
+        <p>{appData.bio.description}</p>
       </div>
     </>
   )
