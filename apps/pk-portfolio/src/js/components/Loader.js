cee770cb8eba312316aee91a8768f68dd153cc80
Paul <paulkaspriskie@gmail.com>
2024-07-21 20:45:28 -0400

    refactor(pk-portfolio): modified ~/components/Loader, updated height and width of loader svg to be more appropriate for use case.

--- apps/pk-portfolio/src/js/components/Loader.js
+++ apps/pk-portfolio/src/js/components/Loader.js
@@ -5,8 +5,8 @@ const Loader = () => {
 	return (
 		<>
 			<svg
-      	width={24}
-      	height={24}
+      	width={110}
+      	height={110}
       	viewBox="0 0 24 24"
       	xmlns="http://www.w3.org/2000/svg">
       	<rect x="1" y="1" rx="1" width="10" height="10">
