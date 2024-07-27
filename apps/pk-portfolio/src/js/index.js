24022698f42c883a9911bcf781a47ede09b55d01
Paul <paulkaspriskie@gmail.com>
2024-07-27 14:00:28 -0400

    refactor(pk-portfolio): modified ~/js/index.js, added logic to dynamically set document title from index js file.

--- apps/pk-portfolio/src/js/index.js
+++ apps/pk-portfolio/src/js/index.js
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { ROUTER } from './router/Router.js';
 import store from './store/store.js';
 import '../scss/index.scss';
+document.title = 'Paul Kaspriskie | Frontend Developer';
 
 
 const root = createRoot(document.getElementById('root'));
