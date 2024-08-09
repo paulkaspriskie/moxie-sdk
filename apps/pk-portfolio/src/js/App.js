232e9c502b307d7eaa9bd6a980d96429f0b4c12b
Paul <paulkaspriskie@gmail.com>
2024-08-09 11:03:55 -0400

    refactor(pk-portfolio): modified ~/App.js & Head.js, migrated head markup and logic from App component to Head component.

--- apps/pk-portfolio/src/js/App.js
+++ apps/pk-portfolio/src/js/App.js
@@ -1,20 +1,13 @@
 import { ROUTES } from './router/Router.js';
 import SideNavigation from './components/SideNavigation.js';
 import Main from './layout/Main.js';
-import {Helmet} from "react-helmet";
-import favicon from '../assets/imgs/favicon.png';
+import Head from './components/Head.js';
 
 
 const App = () => {
   return (
     <div className="app-pk-portfolio__container">
-      <Helmet>
-        <title>Paul Kaspriskie | Frontend Developer</title>
-        <link rel="icon" type="image/png" href={favicon} />
-        <meta name="author" content="Paul Kaspriskie" />
-        <meta name="keywords" content="Paul Kaspriskie, frontend developer, ui developer, javascript, react, html, css" />
-        <meta name="description" content="Paul Kaspriskie Frontend Developer | UI Developer, Over nine years experience building and maintaining modern frontend architectures, ui components and web applications." />
-      </Helmet>
+      <Head />
       <SideNavigation ROUTES={ROUTES} />
       <Main ROUTES={ROUTES}/>
     </div>
--- apps/pk-portfolio/src/js/components/Head.js
+++ apps/pk-portfolio/src/js/components/Head.js
@@ -1,7 +1,19 @@
+import {Helmet} from "react-helmet";
+import favicon from '../../assets/imgs/favicon.png';
+
+
 const Head = () => {
 
   return (
-    <></>
+    <>
+      <Helmet>
+        <title>Paul Kaspriskie | Frontend Developer</title>
+        <link rel="icon" type="image/png" href={favicon} />
+        <meta name="author" content="Paul Kaspriskie" />
+        <meta name="keywords" content="Paul Kaspriskie, frontend developer, ui developer, javascript, react, html, css" />
+        <meta name="description" content="Paul Kaspriskie Frontend Developer | UI Developer, Over nine years experience building and maintaining modern frontend architectures, ui components and web applications." />
+      </Helmet>
+    </>
   );
 
 }
