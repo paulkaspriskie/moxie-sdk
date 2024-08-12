6620c91532b0dfe09ef69de2b0f879dd74a8970f
Paul <paulkaspriskie@gmail.com>
2024-08-12 11:35:00 -0400

    refactor(pk-portfolio): modified ~/assets/imgs, remnamed favicon.png to favicon-32x.png and added sizes attribute to link element in Head component.

--- apps/pk-portfolio/src/js/components/Head.js
+++ apps/pk-portfolio/src/js/components/Head.js
@@ -1,5 +1,5 @@
 import {Helmet} from "react-helmet";
-import favicon from '../../assets/imgs/favicon.png';
+import faviconMed from '../../assets/imgs/favicon-32x.png';
 
 
 const Head = () => {
@@ -8,7 +8,7 @@ const Head = () => {
     <>
       <Helmet>
         <title>Paul Kaspriskie | Frontend Developer</title>
-        <link rel="icon" type="image/png" href={favicon} />
+        <link rel="icon" type="image/png" href={faviconMed} sizes="32x32" />
         <meta name="author" content="Paul Kaspriskie" />
         <meta name="keywords" content="Paul Kaspriskie, frontend developer, ui developer, javascript, react, html, css" />
         <meta name="description" content="Paul Kaspriskie Frontend Developer | UI Developer, Over nine years experience building and maintaining modern frontend architectures, ui components and web applications." />
