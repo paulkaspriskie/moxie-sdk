dac46cf4a374e029c4ee6750db0643691dcbfd19
Paul <paulkaspriskie@gmail.com>
2024-08-12 11:43:05 -0400

    refactor(pk-portfolio): modified ~/components/Head, added link element for 16x16 favicon to Head component.

--- apps/pk-portfolio/src/js/components/Head.js
+++ apps/pk-portfolio/src/js/components/Head.js
@@ -1,4 +1,5 @@
 import {Helmet} from "react-helmet";
+import faviconSm from '../../assets/imgs/favicon-16x.png';
 import faviconMed from '../../assets/imgs/favicon-32x.png';
 
 
@@ -8,6 +9,7 @@ const Head = () => {
     <>
       <Helmet>
         <title>Paul Kaspriskie | Frontend Developer</title>
+        <link rel="icon" type="image/png" href={faviconSm} sizes="16x16" />
         <link rel="icon" type="image/png" href={faviconMed} sizes="32x32" />
         <meta name="author" content="Paul Kaspriskie" />
         <meta name="keywords" content="Paul Kaspriskie, frontend developer, ui developer, javascript, react, html, css" />
